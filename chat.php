608910f17322d59134c94a7eb91f0d501ec3d6e7
Breno-Cardozo <breno_cardozogarcia@hotmail.com>
2023-10-01 21:13:37 -0300

    Alterações

--- chat.php
+++ chat.php
@@ -1,3 +1,12 @@
+<?php
+    session_start();
+    if(!isset($_SESSION ['unique_id'])){
+        header("location: login.php");
+    }
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,11 +20,19 @@
     <div class="wrapper">
         <section class="chat-area">
             <header>
-                <a href="#" class="back-icon"><i class="fa-solid fa-circle-arrow-left" style="color: #bce6e2;"></i></a>
-                <img src="laurinha.jpg" alt="">
+                <?php
+                    include_once "php/config.php";
+                    $user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+                    $sql = mysqli_query($conn,"SELECT * FROM users WHERE unique_id = {$user_id}");
+                    if (mysqli_num_rows($sql) > 0) {
+                        $row = mysqli_fetch_assoc($sql);
+                    }
+                ?>
+                <a href="users.php" class="back-icon"><i class="fa-solid fa-circle-arrow-left" style="color: #bce6e2;"></i></a>
+                <img src="php/images/<?php echo $row['img'] ?>" alt="">
                     <div class="details">
-                        <span>Laura Jeronimo</span>
-                        <p>Online Agora</p>        
+                        <span><?php echo $row['fname'] . " " . $row['lname']; ?></span>
+                        <p><?php echo $row ['status'] ?></p>        
                     </div>
             </header> 
             <div class="chat-box">
@@ -54,10 +71,15 @@
      </div>
     </div>
     <form action="#" class="typing-area">
-        <input type="text" placeholder="Escreva sua mensagem aqui...">
+        <input type="text" name="outgoing_id" value="<?php echo $_SESSION['unique_id']; ?>" hidden>
+        <input type="text" name="incoming_id" value="<?php echo $user_id; ?>" hidden>
+        <input type="text" name="message" class="input-field" placeholder="Escreva sua mensagem aqui...">
         <button><i class="fa-brands fa-telegram" style="color: #bce6e2;"></i></button>
     </form>
         </section>
     </div>
+
+
+    <script src="js/chat.js"></script>
 </body>
 </html>

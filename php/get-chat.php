079c5b0765bf18348e2db611ca5d61633d3b9e7a
Breno-Cardozo <breno_cardozogarcia@hotmail.com>
2023-10-04 21:23:23 -0300

    Finalização do projeto

--- php/get-chat.php
+++ php/get-chat.php
@@ -1,36 +1,37 @@
-<?php
+<?php 
     session_start();
     if(isset($_SESSION['unique_id'])){
         include_once "config.php";
-        $outgoing_id = mysqli_real_escape_string($conn,$_POST['outgoing_id']);
-        $incoming_id = mysqli_real_escape_string($conn,$_POST['incoming_id']);
+        $outgoing_id = $_SESSION['unique_id'];
+        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
         $output = "";
-
-        $sql = "SELECT * FROM messages 
-                LEFT JOIN users ON users.unique_id = messages.incoming_msg_id
-                WHERE (outgoing_msg_id = {$outgoing_id} AND incoming_msg_id  = {$incoming_id})
-                OR (outgoing_msg_id = {$incoming_id} AND incoming_msg_id  = {$outgoing_id}) ORDER BY msg_id ";
+        $sql = "SELECT * FROM messages LEFT JOIN users ON users.unique_id = messages.outgoing_msg_id
+                WHERE (outgoing_msg_id = {$outgoing_id} AND incoming_msg_id = {$incoming_id})
+                OR (outgoing_msg_id = {$incoming_id} AND incoming_msg_id = {$outgoing_id}) ORDER BY msg_id";
         $query = mysqli_query($conn, $sql);
         if(mysqli_num_rows($query) > 0){
             while($row = mysqli_fetch_assoc($query)){
-                if($row['outgoing_msg_id']=== $outgoing_id){
+                if($row['outgoing_msg_id'] === $outgoing_id){
                     $output .= '<div class="chat outgoing">
-                                    <div class="details">
-                                        <p>'. $row['msg'] .'</p>
-                                    </div>
+                                <div class="details">
+                                    <p>'. $row['msg'] .'</p>
+                                </div>
                                 </div>';
-                }
-                else{
+                }else{
                     $output .= '<div class="chat incoming">
-                                        <div class="details">
-                                            <p>'. $row['msg'] .'</p>
+                                <img src="php/images/'.$row['img'].'" alt="">
+                                <div class="details">
+                                    <p>'. $row['msg'] .'</p>
+                                </div>
                                 </div>';
                 }
             }
-            echo $output;
+        }else{
+            $output .= '<div class="text">Envie alguma mensagem para o seu amigo e inicie uma conversa!</div>';
         }
-    else{
-       header("../login.php");
+        echo $output;
+    }else{
+        header("location: ../login.php");
     }
-}
+
 ?>
